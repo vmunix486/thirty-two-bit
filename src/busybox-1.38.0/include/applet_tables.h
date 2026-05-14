7b23349c8f2ed55fceca89443c1ec6ea4c5aa575
Marco Esquandolas <justaguy8086@gmail.com>
2026-05-14 13:13:49 -0400

    Fix a lot of things and also get Makefile somewhat working. Have to figure out why ld is crashing out about i386 and x86-64 crap.

---
+++ src/busybox-1.38.0/include/applet_tables.h
@@ -0,0 +1,353 @@
+/* This is a generated file, don't edit */
+
+#define NUM_APPLETS 112
+#define KNOWN_APPNAME_OFFSETS 4
+
+const uint16_t applet_nameofs[] ALIGN2 = {
+138,
+319,
+490,
+};
+
+const char applet_names[] ALIGN1 = ""
+"[" "\0"
+"[[" "\0"
+"arp" "\0"
+"ash" "\0"
+"awk" "\0"
+"bash" "\0"
+"blkid" "\0"
+"cat" "\0"
+"chgrp" "\0"
+"chmod" "\0"
+"chown" "\0"
+"clear" "\0"
+"comm" "\0"
+"cp" "\0"
+"crc32" "\0"
+"cttyhack" "\0"
+"cut" "\0"
+"date" "\0"
+"dd" "\0"
+"df" "\0"
+"dmesg" "\0"
+"dos2unix" "\0"
+"du" "\0"
+"echo" "\0"
+"env" "\0"
+"false" "\0"
+"fdisk" "\0"
+"find" "\0"
+"free" "\0"
+"ftpget" "\0"
+"getfattr" "\0"
+"grep" "\0"
+"halt" "\0"
+"hdparm" "\0"
+"head" "\0"
+"hexedit" "\0"
+"hostname" "\0"
+"httpd" "\0"
+"ifconfig" "\0"
+"init" "\0"
+"ip" "\0"
+"ipaddr" "\0"
+"iplink" "\0"
+"iproute" "\0"
+"kill" "\0"
+"less" "\0"
+"ln" "\0"
+"losetup" "\0"
+"ls" "\0"
+"lsblk" "\0"
+"makedevs" "\0"
+"mkdir" "\0"
+"mkdosfs" "\0"
+"mkfifo" "\0"
+"mkfs.vfat" "\0"
+"mknod" "\0"
+"mkswap" "\0"
+"mktemp" "\0"
+"mount" "\0"
+"mv" "\0"
+"nc" "\0"
+"netcat" "\0"
+"nice" "\0"
+"nohup" "\0"
+"partprobe" "\0"
+"pidof" "\0"
+"ping" "\0"
+"pivot_root" "\0"
+"poweroff" "\0"
+"printf" "\0"
+"ps" "\0"
+"pstree" "\0"
+"pwd" "\0"
+"readlink" "\0"
+"realpath" "\0"
+"reboot" "\0"
+"reset" "\0"
+"rm" "\0"
+"rmdir" "\0"
+"route" "\0"
+"sed" "\0"
+"seedrng" "\0"
+"seq" "\0"
+"sh" "\0"
+"sha384sum" "\0"
+"sleep" "\0"
+"sort" "\0"
+"ssl_server" "\0"
+"stat" "\0"
+"stty" "\0"
+"sync" "\0"
+"tail" "\0"
+"tee" "\0"
+"telnet" "\0"
+"test" "\0"
+"timeout" "\0"
+"top" "\0"
+"touch" "\0"
+"tr" "\0"
+"tree" "\0"
+"tsort" "\0"
+"udhcpc" "\0"
+"uevent" "\0"
+"umount" "\0"
+"uname" "\0"
+"unix2dos" "\0"
+"uuidgen" "\0"
+"vi" "\0"
+"vmstat" "\0"
+"watch" "\0"
+"wc" "\0"
+"wget" "\0"
+;
+
+#define APPLET_NO_arp 2
+#define APPLET_NO_ash 3
+#define APPLET_NO_awk 4
+#define APPLET_NO_bash 5
+#define APPLET_NO_blkid 6
+#define APPLET_NO_cat 7
+#define APPLET_NO_chgrp 8
+#define APPLET_NO_chmod 9
+#define APPLET_NO_chown 10
+#define APPLET_NO_clear 11
+#define APPLET_NO_comm 12
+#define APPLET_NO_cp 13
+#define APPLET_NO_crc32 14
+#define APPLET_NO_cttyhack 15
+#define APPLET_NO_cut 16
+#define APPLET_NO_date 17
+#define APPLET_NO_dd 18
+#define APPLET_NO_df 19
+#define APPLET_NO_dmesg 20
+#define APPLET_NO_dos2unix 21
+#define APPLET_NO_du 22
+#define APPLET_NO_echo 23
+#define APPLET_NO_env 24
+#define APPLET_NO_false 25
+#define APPLET_NO_fdisk 26
+#define APPLET_NO_find 27
+#define APPLET_NO_free 28
+#define APPLET_NO_ftpget 29
+#define APPLET_NO_getfattr 30
+#define APPLET_NO_grep 31
+#define APPLET_NO_halt 32
+#define APPLET_NO_hdparm 33
+#define APPLET_NO_head 34
+#define APPLET_NO_hexedit 35
+#define APPLET_NO_hostname 36
+#define APPLET_NO_httpd 37
+#define APPLET_NO_ifconfig 38
+#define APPLET_NO_init 39
+#define APPLET_NO_ip 40
+#define APPLET_NO_ipaddr 41
+#define APPLET_NO_iplink 42
+#define APPLET_NO_iproute 43
+#define APPLET_NO_kill 44
+#define APPLET_NO_less 45
+#define APPLET_NO_ln 46
+#define APPLET_NO_losetup 47
+#define APPLET_NO_ls 48
+#define APPLET_NO_lsblk 49
+#define APPLET_NO_makedevs 50
+#define APPLET_NO_mkdir 51
+#define APPLET_NO_mkdosfs 52
+#define APPLET_NO_mkfifo 53
+#define APPLET_NO_mknod 55
+#define APPLET_NO_mkswap 56
+#define APPLET_NO_mktemp 57
+#define APPLET_NO_mount 58
+#define APPLET_NO_mv 59
+#define APPLET_NO_nc 60
+#define APPLET_NO_netcat 61
+#define APPLET_NO_nice 62
+#define APPLET_NO_nohup 63
+#define APPLET_NO_partprobe 64
+#define APPLET_NO_pidof 65
+#define APPLET_NO_ping 66
+#define APPLET_NO_pivot_root 67
+#define APPLET_NO_poweroff 68
+#define APPLET_NO_printf 69
+#define APPLET_NO_ps 70
+#define APPLET_NO_pstree 71
+#define APPLET_NO_pwd 72
+#define APPLET_NO_readlink 73
+#define APPLET_NO_realpath 74
+#define APPLET_NO_reboot 75
+#define APPLET_NO_reset 76
+#define APPLET_NO_rm 77
+#define APPLET_NO_rmdir 78
+#define APPLET_NO_route 79
+#define APPLET_NO_sed 80
+#define APPLET_NO_seedrng 81
+#define APPLET_NO_seq 82
+#define APPLET_NO_sh 83
+#define APPLET_NO_sha384sum 84
+#define APPLET_NO_sleep 85
+#define APPLET_NO_sort 86
+#define APPLET_NO_ssl_server 87
+#define APPLET_NO_stat 88
+#define APPLET_NO_stty 89
+#define APPLET_NO_sync 90
+#define APPLET_NO_tail 91
+#define APPLET_NO_tee 92
+#define APPLET_NO_telnet 93
+#define APPLET_NO_test 94
+#define APPLET_NO_timeout 95
+#define APPLET_NO_top 96
+#define APPLET_NO_touch 97
+#define APPLET_NO_tr 98
+#define APPLET_NO_tree 99
+#define APPLET_NO_tsort 100
+#define APPLET_NO_udhcpc 101
+#define APPLET_NO_uevent 102
+#define APPLET_NO_umount 103
+#define APPLET_NO_uname 104
+#define APPLET_NO_unix2dos 105
+#define APPLET_NO_uuidgen 106
+#define APPLET_NO_vi 107
+#define APPLET_NO_vmstat 108
+#define APPLET_NO_watch 109
+#define APPLET_NO_wc 110
+#define APPLET_NO_wget 111
+
+#ifndef SKIP_applet_main
+int (*const applet_main[])(int argc, char **argv) = {
+test_main,
+test_main,
+arp_main,
+ash_main,
+awk_main,
+ash_main,
+blkid_main,
+cat_main,
+chgrp_main,
+chmod_main,
+chown_main,
+clear_main,
+comm_main,
+cp_main,
+cksum_main,
+cttyhack_main,
+cut_main,
+date_main,
+dd_main,
+df_main,
+dmesg_main,
+dos2unix_main,
+du_main,
+echo_main,
+env_main,
+false_main,
+fdisk_main,
+find_main,
+free_main,
+ftpgetput_main,
+getfattr_main,
+grep_main,
+halt_main,
+hdparm_main,
+head_main,
+hexedit_main,
+hostname_main,
+httpd_main,
+ifconfig_main,
+init_main,
+ip_main,
+ipaddr_main,
+iplink_main,
+iproute_main,
+kill_main,
+less_main,
+ln_main,
+losetup_main,
+ls_main,
+lsblk_main,
+makedevs_main,
+mkdir_main,
+mkfs_vfat_main,
+mkfifo_main,
+mkfs_vfat_main,
+mknod_main,
+mkswap_main,
+mktemp_main,
+mount_main,
+mv_main,
+nc_main,
+nc_main,
+nice_main,
+nohup_main,
+partprobe_main,
+pidof_main,
+ping_main,
+pivot_root_main,
+halt_main,
+printf_main,
+ps_main,
+pstree_main,
+pwd_main,
+readlink_main,
+realpath_main,
+halt_main,
+reset_main,
+rm_main,
+rmdir_main,
+route_main,
+sed_main,
+seedrng_main,
+seq_main,
+ash_main,
+md5_sha1_sum_main,
+sleep_main,
+sort_main,
+ssl_server_main,
+stat_main,
+stty_main,
+sync_main,
+tail_main,
+tee_main,
+telnet_main,
+test_main,
+timeout_main,
+top_main,
+touch_main,
+tr_main,
+tree_main,
+tsort_main,
+udhcpc_main,
+uevent_main,
+umount_main,
+uname_main,
+dos2unix_main,
+uuidgen_main,
+vi_main,
+vmstat_main,
+watch_main,
+wc_main,
+wget_main,
+};
+#endif
+
--- src/linux-3.7.10/graybuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-export GR_CPUS=$(nproc --all)
-
-make -j"$GR_CPUS" ARCH=i386 bzImage
