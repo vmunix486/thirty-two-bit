728ccb49e1accac09f14ac141af5780ee16f3d44
Marco Esquandolas <justaguy8086@gmail.com>
2026-05-14 20:24:33 -0400

    Add some more stuff (i forgot)

--- src/busybox-1.38.0/include/autoconf.h
+++ src/busybox-1.38.0/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.38.0
  */
-#define AUTOCONF_TIMESTAMP "2026-05-14 19:06:25 EDT"
+#define AUTOCONF_TIMESTAMP "2026-05-14 19:48:18 EDT"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
