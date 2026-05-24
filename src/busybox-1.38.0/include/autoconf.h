18712d12cfc4f164932eafb9e31180f5051e2720
Marco Esquandolas <justaguy8086@gmail.com>
2026-05-24 00:08:42 -0400

    Add basic index.html page for busybox httpd, add starthttpd script, and add some things to the extapps makefile

--- src/busybox-1.38.0/include/autoconf.h
+++ src/busybox-1.38.0/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.38.0
  */
-#define AUTOCONF_TIMESTAMP "2026-05-22 16:40:25 EDT"
+#define AUTOCONF_TIMESTAMP "2026-05-23 15:03:22 EDT"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
